48846a99ca4e8d62e16386837fe4b23739823132
Aborevsky01 <45879542+Aborevsky01@users.noreply.github.com>
2018-12-26 18:36:41 +0300

    Create bubbleUnordered.py

---
+++ tests/bubbleUnordered.py
@@ -0,0 +1,17 @@
+from timeit import default_timer as timer
+from src import bubble
+res = []
+
+with open(r'src_tests\unordered.dat', 'r') as file:
+    for line in file:
+        arr1 = [int(x) for x in line.rstrip().split(' ')]
+        s = timer()
+        bubble.bubbleSort(arr1)
+        e = timer()
+        res.append('Bubble ' + str(len(arr1)) + ' ' + str(e-s) + ' seconds\n')
+file.close()
+
+with open(r'res.dat', 'a') as file:
+    file.write('\nUnordered elements from 0 to N\n')
+    for i in res:
+        file.write(i)
